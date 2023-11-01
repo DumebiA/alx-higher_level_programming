d7e4fccfd9ab2a0d5ba7f76aac642208d5528511
root <anyamkpa.ifeanyi@gmail.com>
2023-11-01 09:35:55 -0700

    8 py

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+class Rectangle:
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self._height = value
+
+    def area(self):
+        return self._width * self._height
+
+    def perimeter(self):
+        if self._width == 0 or self._height == 0:
+            return 0
+        return 2 * (self._width + self._height)
+
+    def __str__(self):
+        if self._width == 0 or self._height == 0:
+            return ""
+        return '\n'.join(['#' * self._width for _ in range(self._height)])
+
+    def __repr__(self):
+        return f"Rectangle({self._width}, {self._height})"
+
+    def __del__(self):
+        print(f"Bye rectangle...")
+
+# Sample usage
+my_rectangle = Rectangle(2, 4)
+print(str(my_rectangle))
+print("--")
+print(my_rectangle)
+print("--")
+print(repr(my_rectangle))
+print("--")
+print(hex(id(my_rectangle)))
+print("--")
+
+# Create a new instance based on the representation
+new_rectangle = eval(repr(my_rectangle))
+print(str(new_rectangle))
+print("--")
+print(new_rectangle)
+print("--")
+print(repr(new_rectangle))
+print("--")
+print(hex(id(new_rectangle)))
+print("--")
+
+print(new_rectangle is my_rectangle)
+print(type(new_rectangle) is type(my_rectangle))
