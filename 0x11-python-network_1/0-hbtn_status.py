32ef0d69b9eebad6bc6ee9b93c07a142c15a4512
DumebiA <anyamkpa.ifeanyi@gmail.com>
2024-01-26 23:33:17 +0000

    00

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""  fetches https://intranet.hbtn.io/status  """
 import urllib.request
 
+url = 'https://alx-intranet.hbtn.io/status'
 
-if __name__ == "__main__":
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-        r = response.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(r)))
-        print("\t- content: {}".format(r))
-        print("\t- utf8 content: {}".format(r.decode("UTF-8")))
+try:
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print("\t- Body response:")
+        print("\t\t- type:", type(body))
+        print("\t\t- content:", body)
+except urllib.error.HTTPError as e:
+    print("Error code:", e.code)
